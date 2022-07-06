f4225b9b483a7c4e8f4acc3ac9a6454dad9ed58d
Sid-Ahmed_MIR_Unblocked <smir@unblocked-group.com>
2022-07-06 22:04:16 +0200

    add agent

--- Real-Estate-Website/src/pages/add-agent.js
+++ Real-Estate-Website/src/pages/add-agent.js
@@ -1,39 +1,33 @@
 import React from "react";
-import {Section, Add, Form} from "../components";
-import {useParams} from "react-router-dom";
-import {HeaderContainer, DashboardContainer, FooterContainer} from "../containers";
-import {Description, Media} from "../partials/add_property_agent";
+import { Section, Add, Form } from "../components";
+import { useParams } from "react-router-dom";
+import {
+  HeaderContainer,
+  DashboardContainer,
+  FooterContainer,
+} from "../containers";
+import { Description } from "../partials/add_property_agent";
 
 const AddAgent = () => {
-    const {id} = useParams();
-    return (
-        <>
-            <HeaderContainer bg="false"/>
-            <Section bgColor="--bs-fade-info">
-                <Section.InnerContainer>
-                    <DashboardContainer title={id ? "Edit Property" : "Add Property"}>
-                        <Add>
-                            <Form>
-                                <Description/>
-                                <Media/>
-                                <Add.Footer>
-                                    <Form.FormGroup class="form-group">
-                                        <Form.SubmitInput type="submit" value={id ? "Update Property" : "Submit Property"}/>
-                                    </Form.FormGroup>
-                                    {id && (
-                                        <Form.FormGroup class="form-group">
-                                            <Form.SubmitInput type="submit" value="Cancel Update"/>
-                                        </Form.FormGroup>
-                                    )}
-                                </Add.Footer>
-                            </Form>
-                        </Add>
-                    </DashboardContainer>
-                </Section.InnerContainer>
-            </Section>
-            <FooterContainer/>
-        </>
-    );
+  const { id } = useParams();
+  return (
+    <>
+      <HeaderContainer bg="false" />
+      <Section bgColor="--bs-fade-info">
+        <Section.InnerContainer>
+          <DashboardContainer title={id ? "Edit Agent" : "Add Agent"}>
+            <Add>
+              <Form>
+                <Description />
+                <Add.Footer></Add.Footer>
+              </Form>
+            </Add>
+          </DashboardContainer>
+        </Section.InnerContainer>
+      </Section>
+      <FooterContainer />
+    </>
+  );
 };
 
 export default AddAgent;
--- Real-Estate-Website/src/partials/add_property_agent.js
+++ Real-Estate-Website/src/partials/add_property_agent.js
@@ -1,20 +1,35 @@
-import { Add, Form } from "../components";
-import React, { useState, useRef } from "react";
 import { toast } from "react-toastify";
+import React, { Fragment, useState } from "react";
+import { HeaderContainer, FooterContainer } from "../containers";
+import { Signup, Form } from "../components";
+import { Link, Redirect } from "react-router-dom";
+import { TezosToolkit } from "@taquito/taquito";
+import { BeaconWallet } from "@taquito/beacon-wallet";
 
 const Description = ({ setAuth }) => {
   const [inputs, setInputs] = useState({
+    email: "",
+    password: "",
     name: "",
+    surname: "",
     address: "",
-    type: "",
-    id_agence: "",
-    id_status: "",
-    value: "",
-    max_refunds: "",
+    age: "",
+    salary: "",
+    contrat_type: "",
+    id_agency: "",
   });
 
-  const { name, surname, mail, address, age, salary, contrat_type, id_agency } =
-    inputs;
+  const {
+    name,
+    surname,
+    email,
+    address,
+    age,
+    salary,
+    contrat_type,
+    id_agency,
+    password,
+  } = inputs;
 
   const onChange = (e) =>
     setInputs({ ...inputs, [e.target.name]: e.target.value });
@@ -22,18 +37,30 @@ const Description = ({ setAuth }) => {
   const onSubmitForm = async (e) => {
     e.preventDefault();
     try {
-      const body = { name };
+      const body = {
+        name,
+        surname,
+        email,
+        address,
+        age,
+        salary,
+        contrat_type,
+        id_agency,
+        password,
+      };
       console.log(body);
       const response = await fetch(
-        "http://localhost:5000/authentication/add_asset",
+        "http://localhost:5000/authentication/register",
         {
           method: "POST",
-          headers: { "Content-type": "application/json" },
+          headers: {
+            "Content-type": "application/json",
+          },
           body: JSON.stringify(body),
         }
       );
       const parseRes = await response.json();
-
+      console.log(parseRes.jwtToken);
       if (parseRes.jwtToken) {
         localStorage.setItem("token", parseRes.jwtToken);
         setAuth(true);
@@ -46,156 +73,126 @@ const Description = ({ setAuth }) => {
       console.error(err.message);
     }
   };
+  let addressUser;
+  const rpc = "https://ithacanet.tezos.marigold.dev/";
+  async function connect_wallet() {
+    const Tezos = new TezosToolkit(rpc);
+    const wallet = new BeaconWallet({ name: "Beacon Docs Taquito" });
+
+    Tezos.setWalletProvider(wallet);
+    try {
+      console.log("Requesting permissions...");
+      const permissions = await wallet.client.requestPermissions();
+      console.log("Got permissions:", permissions.address);
+      addressUser = <p>{permissions.address}</p>;
+    } catch (error) {
+      console.log("Got error:", error);
+    }
+  }
   return (
-    <Form onSubmit={onSubmitForm}>
-      <Form.FormGroup>
-        <Form.Label>name</Form.Label>
-        <Form.Input
-          type="text"
-          name="name"
-          value={name}
-          placeholder="name"
-          onChange={(e) => onChange(e)}
-        />
-      </Form.FormGroup>
-      <Form.FormGroup>
-        <Form.Label>surname</Form.Label>
-        <Form.Input
-          type="text"
-          name="surname"
-          value={surname}
-          placeholder="surname"
-          onChange={(e) => onChange(e)}
-        />
-      </Form.FormGroup>
-      <Form.FormGroup>
-        <Form.Label>mail</Form.Label>
-        <Form.Input
-          type="text"
-          name="mail"
-          value={mail}
-          placeholder="mail"
-          onChange={(e) => onChange(e)}
-        />
-      </Form.FormGroup>
-      <Form.FormGroup>
-        <Form.Label>address</Form.Label>
-        <Form.Input
-          type="text"
-          name="address"
-          value={address}
-          placeholder="address"
-          onChange={(e) => onChange(e)}
-        />
-      </Form.FormGroup>
-      <Form.FormGroup>
-        <Form.Label>age</Form.Label>
-        <Form.Input
-          type="text"
-          name="age"
-          value={age}
-          placeholder="age"
-          onChange={(e) => onChange(e)}
-        />
-      </Form.FormGroup>
-      <Form.FormGroup>
-        <Form.Label>salary</Form.Label>
-        <Form.Input
-          type="text"
-          name="salary"
-          value={salary}
-          placeholder="salary"
-          onChange={(e) => onChange(e)}
-        />
-      </Form.FormGroup>
-      <Form.FormGroup>
-        <Form.Label>contrat_type</Form.Label>
-        <Form.Input
-          type="text"
-          name="contrat_type"
-          value={contrat_type}
-          placeholder="contrat_type"
-          onChange={(e) => onChange(e)}
-        />
-      </Form.FormGroup>
-      <Form.FormGroup>
-        <Form.Label>id_agency</Form.Label>
-        <Form.Input
-          type="text"
-          name="id_agency"
-          value={id_agency}
-          placeholder="id_agency"
-          onChange={(e) => onChange(e)}
-        />
-      </Form.FormGroup>
-      <Form.FormGroup>
-        <Form.SubmitInput type="submit" value="Register" />
-      </Form.FormGroup>
-    </Form>
-  );
-};
-
-const Media = () => {
-  const hiddenFileInput = useRef(null);
-
-  const handleFileButton = (e) => {
-    e.preventDefault();
-    hiddenFileInput.current.click();
-  };
-
-  return (
-    <Add.Media>
-      <Add.MediaHeader>
-        <Add.Title>Property Images</Add.Title>
-      </Add.MediaHeader>
-      <Add.MediaContent>
+    <Fragment>
+      <Form onSubmit={onSubmitForm}>
         <Form.FormGroup>
-          <Form.Label>Images</Form.Label>
-          <input
-            type="file"
-            ref={hiddenFileInput}
-            style={{ display: "none" }}
+          <Form.Label>Name</Form.Label>
+          <Form.Input
+            type="text"
+            name="name"
+            value={name}
+            placeholder="name"
+            onChange={(e) => onChange(e)}
           />
-          <Add.Button onClick={handleFileButton}>Upload Files</Add.Button>
         </Form.FormGroup>
-      </Add.MediaContent>
-    </Add.Media>
+        <Form.FormGroup>
+          <Form.Label>Surame</Form.Label>
+          <Form.Input
+            type="text"
+            name="surname"
+            value={surname}
+            placeholder="surname"
+            onChange={(e) => onChange(e)}
+          />
+        </Form.FormGroup>
+        <Form.FormGroup>
+          <Form.Label>Email</Form.Label>
+          <Form.Input
+            type="text"
+            name="email"
+            value={email}
+            placeholder="email"
+            onChange={(e) => onChange(e)}
+          />
+        </Form.FormGroup>
+        <Form.FormGroup>
+          <Form.Label>Address</Form.Label>
+          <Form.Input
+            type="text"
+            name="address"
+            value={address}
+            placeholder="address"
+            onChange={(e) => onChange(e)}
+          />
+        </Form.FormGroup>
+        <Form.FormGroup>
+          <Form.Label>Age</Form.Label>
+          <Form.Input
+            type="text"
+            name="age"
+            value={age}
+            placeholder="age"
+            onChange={(e) => onChange(e)}
+          />
+        </Form.FormGroup>
+        <Form.FormGroup>
+          <Form.Label>salary</Form.Label>
+          <Form.Input
+            type="text"
+            name="salary"
+            value={salary}
+            placeholder="salary"
+            onChange={(e) => onChange(e)}
+          />
+        </Form.FormGroup>
+        <Form.FormGroup>
+          <Form.Label>contrat_type</Form.Label>
+          <Form.Input
+            type="text"
+            name="contrat_type"
+            value={contrat_type}
+            placeholder="contrat_type"
+            onChange={(e) => onChange(e)}
+          />
+        </Form.FormGroup>
+        <Form.FormGroup>
+          <Form.Label>id_agency</Form.Label>
+          <Form.Input
+            type="text"
+            name="id_agency"
+            value={id_agency}
+            placeholder="id_agency"
+            onChange={(e) => onChange(e)}
+          />
+        </Form.FormGroup>
+        <Form.FormGroup>
+          <Form.Label>Password</Form.Label>
+          <Form.Input
+            type="password"
+            name="password"
+            value={password}
+            placeholder="password"
+            onChange={(e) => onChange(e)}
+          />
+        </Form.FormGroup>
+        <Form.FormGroup>
+          <Form.Label>Confirm Password</Form.Label>
+          <Form.Input type="text" />
+        </Form.FormGroup>
+
+        <Form.FormGroup>
+          <Form.SubmitInput type="submit" value="Add Agent" />
+        </Form.FormGroup>
+      </Form>
+    </Fragment>
   );
 };
 
-const Details = () => {
-  return (
-    <Add.Details>
-      <Add.DetailsHeader>
-        <Add.Title>Property Details</Add.Title>
-      </Add.DetailsHeader>
-      <Add.DetailsContent>
-        <Form.FormGroup>
-          <Form.Label>Rooms</Form.Label>
-          <Form.Input type="text" />
-        </Form.FormGroup>
-        <Form.FormGroup>
-          <Form.Label>Bedrooms</Form.Label>
-          <Form.Input type="text" />
-        </Form.FormGroup>
-        <Form.FormGroup>
-          <Form.Label>Bathrooms</Form.Label>
-          <Form.Input type="text" />
-        </Form.FormGroup>
-        <Form.FormGroup>
-          <Form.Label>Structure Type</Form.Label>
-          <Form.Select name="none" id="" class="form-select">
-            <Form.Option disabled defaultValue>
-              Not Available
-            </Form.Option>
-            <Form.Option>Brick</Form.Option>
-            <Form.Option>Wood</Form.Option>
-            <Form.Option>Cement</Form.Option>
-          </Form.Select>
-        </Form.FormGroup>
-      </Add.DetailsContent>
-    </Add.Details>
-  );
-};
-
-export { Description, Media, Details };
+export { Description };
